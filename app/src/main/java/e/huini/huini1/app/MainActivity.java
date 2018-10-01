2305f7d5da7149c3d0c9c036a953e849e1f8bc69
SuHuini <SuHuini@github.com>
2018-10-01 10:52:49 +0300

    Initial commit

---
+++ app/src/main/java/e/huini/huini1/app/MainActivity.java
@@ -0,0 +1,13 @@
+package e.huini.huini1.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
